ec61342b3cbb68b17f16d4a30714dd63bb164a7c
oozyurt <prfoguz@gmail.com>
2019-06-27 22:18:00 -0500

    write add method

---
+++ src/calculator.java
@@ -0,0 +1,11 @@
+public class calculator {
+
+    public static double add (double num1, double num2){
+        return num1+num2;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(add(20,15));
+    }
+}
