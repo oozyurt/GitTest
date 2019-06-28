cf044f563b2c47ca688c1281aef3dde292149a45
oozyurt <prfoguz@gmail.com>
2019-06-27 22:29:43 -0500

    update

--- src/calculator.java
+++ src/calculator.java
@@ -4,8 +4,16 @@ public class calculator {
         return num1+num2;
     }
 
+    public static double subtract (double num1, double num2){
+        return num1-num2;
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println(add(20,15));
+        System.out.println(subtract(20,15));
     }
+
+
 }
