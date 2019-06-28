00cfb1ac14cedab9c9f8c17ebb2ab7cd1d3a3c5e
oozyurt <44916306+oozyurt@users.noreply.github.com>
2019-06-27 22:45:48 -0500

    add multiply method and update  add method

--- src/calculator.java
+++ src/calculator.java
@@ -1,13 +1,21 @@
 public class calculator {
 
     public static double add (double num1, double num2){
-        return num1+num2;
+        Double result = num1+num2;
+        return result;
     }
 
     public static double subtract (double num1, double num2){
         Double result= num1-num2;
         return result;
     }
+    
+      public static double multiply (double num1, double num2){
+        Double result= num1*num2;
+        return result;
+    }
+    
+    
 
     public static void main(String[] args) {
         System.out.println(add(20,15));
